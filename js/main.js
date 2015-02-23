a15a3b84f99ca1212622624b6442a3446583a6df
Kevin Mason <kev.a.mason@gmail.com>
2015-02-23 03:14:14 -0800

    first iteration complete

--- js/main.js
+++ js/main.js
@@ -3,35 +3,53 @@ $( document ).ready(function() {
 	$(window).on('scroll', function(){
 
 		$(window).scroll(function(){
-			var top = $(this).scrollTop();
-			console.log(top);
-
 			$nav = $('#navigation'); 
 			if ($(this).scrollTop() > 500 && $nav.css('position') != 'fixed')
 			{ 
 				$('#navigation').css({'position': 'fixed', 'top': '0px'}); 
-				$('#about.container-fluid').css({'padding-top': '64px'});
+				$('#about.container-fluid').css({'margin-top': '40px'});
 			}
 			if ($(this).scrollTop() < 500 && $nav.css('position') == 'fixed')
 			{
 				$('#navigation').css({'position': 'static', 'top': '0px'});
-				$('#about.container-fluid').css({'padding-top': '2em'});
+				$('#about.container-fluid').css({'margin-top': '0px'});
 			} 
-
-			
-
-
 		});
 	});
 
-	var $root = $('html, body');
-	$('a').click(function() {
-		var href = $.attr(this, 'href');
-		$root.animate({
-			scrollTop: $(href).offset().top
-		}, 750, function () {
-			window.location.hash = href;
+	// $(document).on("scroll", onScroll);
+
+	$('a[href^="#"]').on('click', function (e) {
+		e.preventDefault();
+		$(document).off("scroll");
+
+		// $('a').each(function () {
+		// 	$(this).removeClass('active');
+		// })
+		// $(this).addClass('active');
+
+		var target = this.hash;
+		$target = $(target);
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top+2
+		}, 500, 'swing', function () {
+			window.location.hash = target;
+			// $(document).on("scroll", onScroll);
 		});
-		return false;
 	});
 });
+
+// function onScroll(event){
+// 	var scrollPosition = $(document).scrollTop();
+// 	$('#navigation a').each(function () {
+// 		var currentLink = $(this);
+// 		var refElement = $(currentLink.attr("href"));
+// 		if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+// 			$('#navigation a').removeClass("active");
+// 			currentLink.addClass("active");
+// 		}
+// 		else {
+// 			currentLink.removeClass("active");
+// 		}
+// 	});
+// }
