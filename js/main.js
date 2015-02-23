09022197bc58fa995960a6bf04933447a83e6592
Kevin Mason <kev.a.mason@gmail.com>
2015-02-22 16:56:43 -0800

    navbar implementation working... no active class

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
 $( document ).ready(function() {
-	$(window).on('scroll', function(){
-		var top = $(window).scrollTop();
-		console.log(top);
 
-		if (top > 500) {
-			$('#navigation').addClass('fixed');
-		}
-		else {
-			$('#navigation').removeClass('fixed');
-		}
+	$(window).on('scroll', function(){
+
+		$(window).scroll(function(){
+			var top = $(this).scrollTop();
+			console.log(top);
+
+			$nav = $('#navigation'); 
+			if ($(this).scrollTop() > 500 && $nav.css('position') != 'fixed')
+			{ 
+				$('#navigation').css({'position': 'fixed', 'top': '0px'}); 
+				$('#about.container-fluid').css({'padding-top': '64px'});
+			}
+			if ($(this).scrollTop() < 500 && $nav.css('position') == 'fixed')
+			{
+				$('#navigation').css({'position': 'static', 'top': '0px'});
+				$('#about.container-fluid').css({'padding-top': '2em'});
+			} 
+
+			
+
+
+		});
+	});
+
+	var $root = $('html, body');
+	$('a').click(function() {
+		var href = $.attr(this, 'href');
+		$root.animate({
+			scrollTop: $(href).offset().top
+		}, 750, function () {
+			window.location.hash = href;
+		});
+		return false;
 	});
 });
